485e060da3281cade97830c9e81932dac478a77e
Dubravka Drčelić <duca.drcelic25@gmail.com>
2024-11-21 20:51:09 +0100

    changed bottom nav and added floating buttons

--- app/src/main/java/com/example/tracker/adapters/FavoritesAdapter.kt
+++ app/src/main/java/com/example/tracker/adapters/FavoritesAdapter.kt
@@ -1,14 +1,30 @@
 package com.example.tracker.adapters
 
 import android.content.Context
+import android.graphics.BitmapFactory
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
+import com.example.tracker.R
+import com.example.tracker.adapters.ProductAdapter.ProductViewHolder
+import com.example.tracker.fragments.FavoritesFragmentDirections
+import com.example.tracker.fragments.HomeFragmentDirections
 import com.example.tracker.models.Product
+import com.google.firebase.storage.FirebaseStorage
 
-class FavoritesAdapter(val context: Context, val favorites: ArrayList<Product>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class FavoritesAdapter(val context: Context, val view:View, private val favorites: ArrayList<Product>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private var storageRef = FirebaseStorage.getInstance().getReference("product_images")
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+        val view: View = LayoutInflater.from(context).inflate(R.layout.product_list_item, parent, false)
+        return FavoritesViewHolder(view)
     }
 
     override fun getItemCount(): Int {
@@ -16,10 +32,34 @@ class FavoritesAdapter(val context: Context, val favorites: ArrayList<Product>) 
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val current = favorites[position]
+        val viewHolder = holder as FavoritesViewHolder
+
+        if(current.image == null){
+            viewHolder.image.setImageResource(R.drawable.baseline_map_24)
+        } else {
+            storageRef.child("/"+current.id).getBytes(10*1024*1024).addOnSuccessListener {
+                val bitmap = BitmapFactory.decodeByteArray(it,0,it.size)
+                viewHolder.image.setImageBitmap(bitmap)
+            }.addOnFailureListener {
+                viewHolder.image.setImageResource(R.drawable.baseline_map_24)
+                Log.w("Firebase Storage","Cannot retrieve image from storage",it)
+            }
+        }
+        viewHolder.name.text = current.name
+        viewHolder.barcode.text = current.barcode
+
+        // Issue with navigation
+//        viewHolder.card.setOnClickListener {
+//            val action = FavoritesFragmentDirections.actionFavoritesFragmentToProductDetailsFragment(current)
+//            Navigation.findNavController(view).navigate(action)
+//        }
     }
 
     class FavoritesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-
+        val image = itemView.findViewById<ImageView>(R.id.ivFavoritesImage)
+        val name = itemView.findViewById<TextView>(R.id.tvFavoritesName)
+        val barcode = itemView.findViewById<TextView>(R.id.tvFavoritesBarcode)
+        val card = itemView.findViewById<CardView>(R.id.cvFavoritesItem)
     }
 }
--- app/src/main/java/com/example/tracker/fragments/FavoritesFragment.kt
+++ app/src/main/java/com/example/tracker/fragments/FavoritesFragment.kt
@@ -1,22 +1,76 @@
 package com.example.tracker.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.tracker.R
+import com.example.tracker.adapters.ProductAdapter
+import com.example.tracker.models.Product
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 
 class FavoritesFragment : Fragment() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: ProductAdapter
+    private lateinit var favList: ArrayList<Product>
+
+    private lateinit var favoritesRef: DatabaseReference
+    private lateinit var storageRef: StorageReference
+    private lateinit var userRef: DatabaseReference
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        (requireActivity() as AppCompatActivity).supportActionBar?.hide()
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_favorites, container, false)
-        (requireActivity() as AppCompatActivity).supportActionBar?.hide()
+
+        // Firebase
+        auth = FirebaseAuth.getInstance()
+        favoritesRef = FirebaseDatabase.getInstance().getReference("favorites")
+        storageRef = FirebaseStorage.getInstance().getReference("product_images")
+        userRef = FirebaseDatabase.getInstance().getReference("users")
+
+        favList = ArrayList<Product>()
+
+        // RecyclerView
+        adapter = ProductAdapter(view.context,view, favList)
+        recyclerView = view.findViewById(R.id.recyclerViewFavorites)
+        recyclerView.layoutManager =
+            LinearLayoutManager(view.context, LinearLayoutManager.VERTICAL, false)
+        recyclerView.adapter = adapter
+
+        favoritesRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                favList.clear()
+                for (productSnapshot in snapshot.children){
+                    val prod = productSnapshot.getValue(Product::class.java)
+                    favList.add(prod!!)
+                }
+                adapter.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.w("getUser","Failed to read value",error.toException())
+            }
+
+        })
+
         return view
     }
 
